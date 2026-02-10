f0008d620faf8a73239428f635bcbf4925e4b84c
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2026-02-10 13:36:04 +0100

    feat: add unit tests for cleanInput using Vitest

---
+++ src/repl.test.ts
@@ -0,0 +1,20 @@
+import { cleanInput } from "./repl.js";
+import { describe, expect, test } from "vitest";
+
+describe.each([
+  { input: "  hello  world  ", expected: ["hello", "world"] },
+  { input: "  Hello  World  ", expected: ["hello", "world"] },
+  { input: "HelLo  World  ", expected: ["hello", "world"] },
+  { input: "  HELLO  World  ", expected: ["hello", "world"] },
+  { input: "  Hello  WorlD    ", expected: ["hello", "world"] },
+])("cleanInput($input)", ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    const actual = cleanInput(input);
+
+    expect(actual).toHaveLength(expected.length);
+
+    expected.forEach((value, index) => {
+      expect(actual[index]).toBe(value);
+    });
+  });
+});
