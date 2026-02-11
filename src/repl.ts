7280c49508426ffcd073eb32d1896dc7c2e18082
Anca Maria Sirbu <ancamaria_sirbu@yahoo.com>
2026-02-11 12:34:02 +0100

    feat: enhance REPL functionality and update .gitignore

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,28 @@
- export function cleanInput(input: string): string[] {
+import readline from "readline";
+
+
+export function cleanInput(input: string): string[] {
   return input.toLowerCase().trim().split(/\s+/);
+}
+
+export function startREPL() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+    rl.prompt();
+
+  rl.on("line", (input: string) => {
+    const words = cleanInput(input);
+
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${words[0]}`);
+    rl.prompt();
+  });
 }
